9997d870041604977205e6ca8cddaf78bafc7ced
Stephanie Youstra <steph.you07@gmail.com>
2018-05-17 21:12:02 -0400

    feat: Create Main Activity layout and Movie class

---
+++ app/src/main/java/com/syoustra/popularmovies/Movie.java
@@ -0,0 +1,72 @@
+/*
+ * PROJECT LICENSE:
+ *
+ * This project has been completed by Stephanie Youstra as part of the Udacity Android Developer Nanodegree.
+ * Feel free to view this as a reference point, but remember Udacity's Honor Code requiring that submissions be original work. Any liability for the consequences of plagiarism lies solely with the "borrower".
+ *
+ * Copyright (c) 2018 ~ Stephanie Youstra.
+ */
+
+package com.syoustra.popularmovies;
+
+public class Movie {
+
+    private String title;
+    private String thumbnail;
+    private String summary;
+    private double rating;
+    private String releaseDate;
+
+
+    public Movie() {
+
+    }
+
+    public Movie(String title, String thumbnail, String summary, double rating, String releaseDate) {
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.summary = summary;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
